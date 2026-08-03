94e9cf064c7e51eaa3a4e3953b4bb5f53170befb
nipunchamara <83004320+nipunchamara@users.noreply.github.com>
2026-08-03 23:37:14 +0530

    update

--- Portfolio-React-Tailwind-main/src/components/Hero.jsx
+++ Portfolio-React-Tailwind-main/src/components/Hero.jsx
@@ -25,12 +25,12 @@ const Hero = () => {
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
                 transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
           onClick={async () => {
-            const response = await fetch('/W.N.C.K.%20Warnakulasooriya.pdf');
+            const response = await fetch('/W.N.C.K.%20Warnakulasooriya%20Resume.pdf');
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
             link.href = url;
-            link.download = 'W.N.C.K. Warnakulasooriya.pdf';
+            link.download = 'W.N.C.K. Warnakulasooriya Resume.pdf';
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
